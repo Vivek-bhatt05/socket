d95b0af3953d1620b8544d3083cbf02e9a44a4be
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-08-11 12:21:18 +0530

    fdsa

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io-client');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', socket => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('layoutChange', updatedLayout => {
+    // Broadcast the updated layout to all clients except the sender
+    socket.broadcast.emit('updateLayout', updatedLayout);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
