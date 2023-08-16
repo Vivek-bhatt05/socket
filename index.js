13ac89fef8cfc7b8dd009b45b91e600ace5c7767
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-08-16 11:11:13 +0530

    er

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io-client');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+app.use(cors);
+
 
 io.on('connection', socket => {
   console.log('Client connected:', socket.id);
